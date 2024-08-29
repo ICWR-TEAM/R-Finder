58c0899f1341e331f159d8cd2843e9c6a848efbf
Afrizal F.A <77115748+afrzlfa@users.noreply.github.com>
2024-08-29 09:11:18 +0700

    Update R-Finder.py

--- R-Finder.py
+++ R-Finder.py
@@ -45,13 +45,11 @@ class finder:
     def useragent(self):
 
         arr = [
-
             "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.0.12) Gecko/2009070611 Firefox/3.0.12 (.NET CLR 3.5.30729)",
             "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.464.0 Safari/534.3",
             "Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10_5_8; ja-jp) AppleWebKit/533.16 (KHTML, like Gecko) Version/5.0 Safari/533.16",
             "Mozilla/5.0 (X11; U; FreeBSD i386; en-US) AppleWebKit/532.0 (KHTML, like Gecko) Chrome/4.0.207.0 Safari/532.0",
             "Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/534.1 (KHTML, like Gecko) Chrome/6.0.427.0 Safari/534.1"
-
         ]
 
         return arr[random.randint(0, len(arr) - 1)]
@@ -62,20 +60,17 @@ class finder:
 
             head = "GET /{} HTTP/1.1\r\n".format(path)
             head += "Host: {}\r\n".format(self.args.target.split("/")[2])
-            head += "User-Agent: {}\r\n".format(self.useragent)
+            head += "User-Agent: {}\r\n".format(self.useragent())
             head += "Connection: Keep-Alive\r\n"
             head += "Accept: */*\r\n\r\n"
 
             if self.args.target.split(":")[0] == "https":
-
                 get_ssl = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-                s = get_ssl.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM), server_hostname = self.args.target.split("/")[2])
-                port = 443
-
-            elif self.args.target.split(":")[0] == "http":
-
+                s = get_ssl.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM), server_hostname=self.args.target.split("/")[2])
+            else:
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                port = 80
+
+            port = self.args.port if self.args.port else (443 if self.args.target.split(":")[0] == "https" else 80)
 
             s.settimeout(self.args.timeout)
             s.connect((self.args.target.split("/")[2], port))
@@ -83,13 +78,11 @@ class finder:
             response = s.recv(50).decode()
             s.close()
 
-            if ("200" in response):
-
+            if "200" in response:
                 print("\n[+] {}/{}\n".format(self.args.target, path))
                 self.result += "[+] {}/{}\n".format(self.args.target, path)
 
-        except:
-
+        except Exception as e:
             pass
 
         self.percentageProc()
@@ -102,10 +95,11 @@ class finder:
         self.procDone = 0
         self.result = ""
         parser = ArgumentParser()
-        parser.add_argument("-x", "--target", required = True)
-        parser.add_argument("-l", "--list", required = True)
-        parser.add_argument("-t", "--thread", required = True, type = int)
-        parser.add_argument("-d", "--timeout", required = True, type = int)
+        parser.add_argument("-x", "--target", required=True)
+        parser.add_argument("-l", "--list", required=True)
+        parser.add_argument("-t", "--thread", required=True, type=int)
+        parser.add_argument("-d", "--timeout", required=True, type=int)
+        parser.add_argument("-p", "--port", required=False, type=int, help="Specify a custom port (optional)")
         self.args = parser.parse_args()
 
         print("[*] R-Finder Running")
